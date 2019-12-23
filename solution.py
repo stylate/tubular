158770ea9a21b8fd1c51c7f79a0f22bb71410209
Alan Nguyen <nguyenalan@berkeley.edu>
2019-12-23 14:36:17 -0800

    fix bug

--- solution.py
+++ solution.py
@@ -1,10 +1,16 @@
+#!/usr/bin/python3
+# Alan Nguyen - Tubular Labs Coding Challenge
 import sys
 
 def find_compounds(word, components):
-    '''
+    """
     For a select word WORD, determine if it is a composite word
     (i.e. it is composed of 2 or more words in set S).
-    '''
+
+    Parameters:
+        word (string): input word
+        components (set): set of component words
+    """
     if not word:
         return True # base case
     for i in range(1, len(word) + 1):
@@ -18,28 +24,30 @@ def find_compounds(word, components):
     return False
 
 def process_list(inputs):
-    '''
+    """
     Process through a list of inputs and return compound words.
-    Time: O(min(nlogn, nk^2)) for k = word length
-    Space: O(2n) => O(n) {both set and output can contain at most n words}.
-    '''
+
+    Parameters:
+        inputs (set): set of unique input words
+    """
     components = set()
-    # compile a sorted list of words?
     output = set()
+    # sort our inputs since compound words are typically longer than component words
     sorted_words = sorted(inputs, key=lambda s: len(s))
     for word in sorted_words:
-        if find_compounds(word, components): output.add(w)
+        if find_compounds(word, components): output.add(word)
     output = sorted(output) # outout expected to be sorted
     return output
 
 if __name__ == '__main__':
     words = set()
-    # clean duplicates from input before processing algorithm
+    # clean stdin before processing algorithm
     for line in sys.stdin:
         w = line.strip() # strips newline and whitespace
         if len(w) > 0 and w not in words: # no empty strings
             words.add(w)
     compounds = process_list(words)
+
     # print results to stdout
     for c in compounds:
         print(c)
