1ed802eb7e3b78d1738637fbe572a26a85f6615d
Alan Nguyen <nguyenalan@berkeley.edu>
2019-12-23 12:49:09 -0800

    stick with processlist func

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 import time
 import sys
 
-def findComposites(word, s):
+def findCompounds(word, s):
     '''
     For a select word WORD, determine if it is a composite word
     (i.e. it is composed of 2 or more words in set S).
@@ -13,7 +13,8 @@ def findComposites(word, s):
         # perform recursion on the SUFFIX.
         if prefix in s and findComposites(suffix, s):
             return True
-    s.add(word) # add word to s (word is a component).
+    s.add(word)
+    print("set: ", s)
     return False
 
 def processList(words):
@@ -27,23 +28,7 @@ def processList(words):
     # compile a sorted list of words?
     output = set()
     for w in sorted(words, key=lambda s: len(s)):
-        if findComposites(w, s): output.add(w)
-    output = sorted(output)
-    elapsed = time.process_time() - t
-    print("time taken: ", elapsed)
-    return output
-
-def fasterList(words):
-    '''
-    Trade time for potentially more space (one less sorting operation done). This can potentially be hazardous since we initialize our set to contain ALL words at the very beginning.
-    '''
-    t = time.process_time()
-    s = set(words)
-    output = set()
-    for w in words:
-        s.remove(w)
-        if findComposites(w, s): output.add(w)
-        s.add(w)
+        if findCompounds(w, s): output.add(w)
     output = sorted(output)
     elapsed = time.process_time() - t
     print("time taken: ", elapsed)
@@ -53,12 +38,10 @@ if __name__ == '__main__':
     words = set()
     # clean duplicates from input before processing algorithm
     for line in sys.stdin:
-        w = line.strip()
+        w = line.strip() # strips newline and whitespace
         if w not in words:
             words.add(w)
-    compound_1 = processList(words)
-    compound_2 = fasterList(words)
-
+    compounds = processList(words)
     # print results to stdout
-    for c in compound_2:
+    for c in compounds:
         print(c)
